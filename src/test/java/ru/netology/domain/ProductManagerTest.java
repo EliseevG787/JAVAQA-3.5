eee9ea01031a328dfe386b3dcd591ad50382c8a2
unknown <rdn787@bk.ru>
2021-02-27 00:55:30 +0300

    updated ProductManagerTest.java

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -2,6 +2,7 @@ package ru.netology.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ru.netology.manager.ProductManager;
 import ru.netology.repository.ProductRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,19 +12,23 @@ class ProductManagerTest {
     ProductManager manager = new ProductManager(repository);
     Product book1 = new Book(1, "Чистый код", 364, "Роберт Мартин");
     Product book2 = new Book(2, "Искусство программирования", 3298, "Дональд Кнут");
-    Product smartphone1 = new Smartphone(3, "iPhone 12", 119990, "Apple");
-    Product smartphone2 = new Smartphone(4, "Galaxy A51", 22290, "Samsung");
+    Product book3 = new Book(3, "Сюрреальные числа", 3298, "Дональд Кнут");
+    Product smartphone1 = new Smartphone(4, "iPhone 12", 119990, "Apple");
+    Product smartphone2 = new Smartphone(5, "Galaxy A51", 22290, "Samsung");
+    Product smartphone3 = new Smartphone(6, "Galaxy M31", 16490, "Samsung");
 
-    @BeforeEach
-    public void setUp(){
-        manager.add(book1);
-        manager.add(book2);
-        manager.add(smartphone1);
-        manager.add(smartphone2);
+    @Test
+    void shouldSearchWhenTheSetIsEmpty() {
+        Product[] actual = manager.searchBy("Чистый код");
+        Product[] expected = new Product[]{};
+
+        assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchBookByName() {
+        manager.add(book1);
+
         Product[] actual = manager.searchBy("Чистый код");
         Product[] expected = new Product[]{book1};
 
@@ -32,14 +37,26 @@ class ProductManagerTest {
 
     @Test
     void shouldSearchBookByAuthor() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
         Product[] actual = manager.searchBy("Дональд Кнут");
-        Product[] expected = new Product[]{book2};
+        Product[] expected = new Product[]{book2, book3};
 
         assertArrayEquals(expected, actual);
     }
 
     @Test
     void shouldSearchByNotExistName() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+
         Product[] actual = manager.searchBy("Паттерны проектирования");
         Product[] expected = new Product[]{};
 
@@ -48,6 +65,8 @@ class ProductManagerTest {
 
     @Test
     void shouldSearchSmartphoneByName() {
+        manager.add(smartphone1);
+
         Product[] actual = manager.searchBy("iPhone 12");
         Product[] expected = new Product[]{smartphone1};
 
@@ -56,8 +75,14 @@ class ProductManagerTest {
 
     @Test
     void shouldSearchSmartphoneByFabricator() {
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(smartphone1);
+        manager.add(smartphone2);
+        manager.add(smartphone3);
+
         Product[] actual = manager.searchBy("Samsung");
-        Product[] expected = new Product[]{smartphone2};
+        Product[] expected = new Product[]{smartphone2, smartphone3};
 
         assertArrayEquals(expected, actual);
     }
